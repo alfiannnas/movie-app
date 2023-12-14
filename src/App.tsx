601d78149904ffe7f40c5e0cc279c5e138d955a6
alfiannnas <alfian.baits@gmail.com>
2023-12-14 19:06:54 +0700

    feat(search bar & card): add 2 feature & make query

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,37 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>;
-}
+const App = () => {
+  const search = (q) => {
+    console.log({ q });
+  };
+
+  return (
+    <div className="flex flex-col items-center h-screen">
+      {/* Search Bar */}
+      <div className="mb-8">
+        <label className="form-control w-full max-w-xs flex">
+          <div className="label">
+            <span className="label-text">Film apa yang sedang kamu cari?</span>
+          </div>
+          <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" onChange={({ target }) => search(target.value)} />
+        </label>
+      </div>
+
+      {/* Card */}
+      <div className="flex justify-start items-start">
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Jsudul</h2>
+            <p>Tanggal</p>
+            <div className="card-actions justify-end">
+              <div className="badge badge-accent">8.9</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
