8f6d3c1edecd07eaa73ce75d91f9a41a52c324df
alfiannnas <alfian.baits@gmail.com>
2023-12-16 00:46:11 +0700

    (style): make card flex-wrap horizontally

--- src/App.tsx
+++ src/App.tsx
@@ -13,17 +13,15 @@ const App = () => {
 
   const PopularMovieList = () => {
     return popularMovies.map((movie, i) => (
-      <div className="flex justify-start items-start" key={i}>
-        <div className="card w-96 bg-base-100 shadow-xl">
-          <figure>
-            <img src={`${import.meta.env.VITE_APP_BASEIMGURL}/${movie.poster_path}`} alt={movie.title} />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">{movie.title}</h2>
-            <p>{movie.release_date}</p>
-            <div className="card-actions justify-end">
-              <div className="badge badge-accent">{movie.vote_average}</div>
-            </div>
+      <div className="card w-56 bg-base-100 shadow-xl" key={i}>
+        <figure className="max-h-290">
+          <img src={`${import.meta.env.VITE_APP_BASEIMGURL}/${movie.poster_path}`} alt={movie.title} />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{movie.title}</h2>
+          <p>{movie.release_date}</p>
+          <div className="card-actions justify-end">
+            <div className="badge badge-accent">{movie.vote_average}</div>
           </div>
         </div>
       </div>
@@ -35,21 +33,22 @@ const App = () => {
   };
 
   return (
-    <div className="flex flex-col items-center h-screen">
-      <p>my secret key = {import.meta.env.VITE_APP_APIKEY}</p>
-      {/* Search Bar */}
-      <div className="mb-8">
-        <label className="form-control w-full max-w-xs flex">
-          <div className="label">
-            <span className="label-text">Film apa yang sedang kamu cari?</span>
-          </div>
-          <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" onChange={({ target }) => search(target.value)} />
-        </label>
+    <>
+      <div className="flex flex-col items-center">
+        {/* Search Bar */}
+        <div className="mb-8">
+          <label className="form-control w-full max-w-xs flex">
+            <div className="label">
+              <span className="label-text">Film apa yang sedang kamu cari?</span>
+            </div>
+            <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" onChange={({ target }) => search(target.value)} />
+          </label>
+        </div>
       </div>
-
-      {/* Card */}
-      <PopularMovieList />
-    </div>
+      <div className="flex flex-wrap gap-20 justify-center">
+        <PopularMovieList />
+      </div>
+    </>
   );
 };
 
